313900ed7a023aa961b7f174e582108e397a8b5c
Acly <aclysia@gmail.com>
2025-06-18 13:34:50 +0200

    Add `ggml_roll` (ggml/1274)
    * ggml : add ggml_roll
    
    * use set/get_op_params & std::min

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -970,6 +970,7 @@ static const char * GGML_OP_NAME[GGML_OP_COUNT] = {
     "PAD",
     "PAD_REFLECT_1D",
     "ROLL",
+    "ROLL",
     "ARANGE",
     "TIMESTEP_EMBEDDING",
     "ARGSORT",
@@ -1002,7 +1003,7 @@ static const char * GGML_OP_NAME[GGML_OP_COUNT] = {
     "GLU",
 };
 
-static_assert(GGML_OP_COUNT == 86, "GGML_OP_COUNT != 86");
+static_assert(GGML_OP_COUNT == 83, "GGML_OP_COUNT != 83");
 
 static const char * GGML_OP_SYMBOL[GGML_OP_COUNT] = {
     "none",
@@ -1102,7 +1103,7 @@ static const char * GGML_OP_SYMBOL[GGML_OP_COUNT] = {
     "glu(x)",
 };
 
-static_assert(GGML_OP_COUNT == 86, "GGML_OP_COUNT != 86");
+static_assert(GGML_OP_COUNT == 83, "GGML_OP_COUNT != 83");
 
 static_assert(GGML_OP_POOL_COUNT == 2, "GGML_OP_POOL_COUNT != 2");
 
